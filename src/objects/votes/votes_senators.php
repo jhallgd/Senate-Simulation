f355f95450195dead3dbaa7ce823c469e87917d5
Jacob Hall <jhall@jahgd.com>
2025-03-04 10:16:51 -0500

    Fixed the Senate Board and added a check for senators with the same last name.

--- src/objects/votes/votes_senators.php
+++ src/objects/votes/votes_senators.php
@@ -7,7 +7,13 @@ class votes_senators extends votes{
     public function __construct(array $data){
         parent::__construct($data);
         $this->se_first_name = $data["se_first_name"];
-        $this->se_last_name = $data["se_last_name"];
+
+        if($data["ct_count"]>1){
+            $this->se_last_name = substr($data["se_first_name"], 0,1).'. '.$data["se_last_name"];
+        }else{
+            $this->se_last_name = $data["se_last_name"];
+        }
+
         $this->vt_color = $data["vt_color"];
 
     }
