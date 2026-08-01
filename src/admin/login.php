f12a0cae52a7c2c20855de8836cb62a24540f23e
Jacob <jhallgd@vt.edu>
2026-08-01 10:13:46 -0400

    Fixed login error.

--- src/admin/login.php
+++ src/admin/login.php
@@ -7,23 +7,16 @@ include_once("admin_header.php");
 echo '<div class="mainContainer">';
 echo '<div class="center_box">';
 
-if(isset($_POST["uname"])){
-    $ad_name = $_POST["uname"];
-}else{
-    $ad_name = -1;
-}
-if(!isset($_POST["pass"])){
-    $ad_name = -1;
+$ad_name = -1;
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['uname'], $_POST['pass'])) {
+    $ad_name = $da->check_admin_login($_POST['uname'], $_POST['pass']);
 }
 
-$ad_name = $da->check_admin_login($_POST['uname'], $_POST['pass']);
-
-if($ad_name == -1){
-    echo '<script>window.location.replace("' . $base_href . 'admin");</script>';
-}else{
+if ($ad_name == -1) {
+    echo '<script>window.location.replace("' . $base_href . 'admin/login_form.php");</script>';
+} else {
     $_SESSION["ad"] = $ad_name;
-    echo '<script>window.location.replace("' . $base_href . 'admin");</script>';
-
+    echo '<script>window.location.replace("' . $base_href . 'admin/");</script>';
 }
 echo '</div>';
 include_once($ROOT . "/components/footer.php");
